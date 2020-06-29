a3247dbd82a40d666ebe720648d588795680abbf
Ridwan Ijadunola <dareedyone@gmail.com>
2020-06-29 11:53:00 +0100

    unicafe 1.6

---
+++ part 1/unicafe 1.6 to 1.11 (commits for each excercises)/src/index.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <h3>Give feedback</h3>
+
+      <div>
+        <button onClick={() => setGood(good + 1)}>Good</button>
+        <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+        <button onClick={() => setBad(bad + 1)}>Bad</button>
+      </div>
+
+      <div>
+        <h3>Statistics</h3>
+
+        <p>Good {good}</p>
+        <p>Neutral {neutral}</p>
+        <p>Bad {bad}</p>
+      </div>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
