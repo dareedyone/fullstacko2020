091281e3d961ef0b4bf717cdeb3a8c6ace05e71a
Ridwan Ijadunola <dareedyone@gmail.com>
2020-07-01 10:32:42 +0100

    2.4: Couse Information Step 9 completed

--- part 2/2.1 to 2.5/src/App.js
+++ part 2/2.1 to 2.5/src/App.js
@@ -7,7 +7,7 @@ const Course = ({ course }) => (
     <Total parts={course.parts} />
   </div>
 );
-const Header = ({ course }) => <h1>{course}</h1>;
+const Header = ({ course }) => <h2>{course}</h2>;
 
 const Part = ({ part }) => (
   <p>
@@ -19,7 +19,7 @@ const Content = ({ parts }) => {
   return (
     <>
       {parts.map((part) => (
-        <Part part={part} />
+        <Part key={part.id} part={part} />
       ))}
     </>
   );
@@ -34,29 +34,59 @@ const Total = ({ parts }) => (
 );
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-    ],
-  };
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
-  return <Course course={course} />;
+  return (
+    <>
+      <h1>Web development curriculum</h1>
+      {courses.map((course) => (
+        <Course key={course.id} course={course} />
+      ))}
+    </>
+  );
 };
 
 export default App;
