e7ade667c149cb1527d32011a7557fe89a0e2917
Ridwan Ijadunola <dareedyone@gmail.com>
2020-07-01 17:27:07 +0100

    2.9*: The Phonebook Step 4 completed

--- part 2/2.6 to 2.10 Phonebook/src/App.js
+++ part 2/2.6 to 2.10 Phonebook/src/App.js
@@ -2,10 +2,15 @@ import React, { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: 24948234828 },
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [peopleToShow, setPeopleToShow] = useState(persons);
+  const [newFilter, setNewFilter] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -18,10 +23,22 @@ const App = () => {
     setNewName("");
   };
 
+  const doFiltering = (e) => {
+    setNewFilter(e.target.value);
+    const re = new RegExp(newFilter, "i");
+    const match = persons.filter((person) => re.test(person.name));
+    match.length > 0 ? setPeopleToShow(match) : setPeopleToShow(persons);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <div>
+        Filter shown with: <input value={newFilter} onChange={doFiltering} />
+      </div>
       <form onSubmit={handleSubmit}>
+        <h3>Add a new</h3>
         <div>
           name:{" "}
           <input value={newName} onChange={(e) => setNewName(e.target.value)} />
@@ -39,7 +56,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(({ name, number }) => (
+        {peopleToShow.map(({ name, number }) => (
           <p key={name}>
             {name} {number}
           </p>
