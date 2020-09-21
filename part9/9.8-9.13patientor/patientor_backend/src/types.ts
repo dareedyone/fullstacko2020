61894ef6fe43b5bdbc2dc4c70afeda153de54b48
Ridwan Ijadunola <dareedyone@gmail.com>
2020-09-21 16:15:42 +0100

    9.19: patientor, step4

--- part9/9.8-9.13patientor/patientor_backend/src/types.ts
+++ part9/9.8-9.13patientor/patientor_backend/src/types.ts
@@ -1,5 +1,32 @@
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {}
+// Diagnose['code'][] or Array<Diagnosis['code']>
+
+interface BaseEntry {
+	id: string;
+	description: string;
+	date: string;
+	specialist: string;
+	diagnosisCodes?: Array<Diagnose["code"]>;
+}
+export enum HealthCheckRating {
+	"Healthy" = 0,
+	"LowRisk" = 1,
+	"HighRisk" = 2,
+	"CriticalRisk" = 3,
+}
+
+interface HealthCheckEntry extends BaseEntry {
+	type: "HealthCheck";
+	healthCheckRating: HealthCheckRating;
+}
+interface OccupationalHealthcareEntry extends BaseEntry {
+	type: "OccupationalHealthcare";
+	employerName: string;
+	sickLeave?: { startDate: string; endDate: string };
+}
+interface HospitalEntry extends BaseEntry {
+	type: "Hospital";
+	discharge: { date: string; criteria: string };
+}
 
 export interface Diagnose {
 	code: string;
@@ -23,3 +50,7 @@ export interface Patient {
 export type NewPatient = Omit<Patient, "id">;
 // export type NonSensitivePatient = Omit<Patient, "ssn">;
 export type PublicPatient = Omit<Patient, "ssn" | "entries">;
+export type Entry =
+	| HospitalEntry
+	| OccupationalHealthcareEntry
+	| HealthCheckEntry;
